f6bb6f78efbb8642588994d3005e30c28a425836
= <=>
2013-12-03 04:02:29 -0600

    Finalize code

--- example/source/app.d
+++ example/source/app.d
@@ -1,24 +1,48 @@
-import std.stdio;
-import recaptcha;
+import std.string;
+import vibe.d;
+import recaptcha.connect;
 
 void index(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.render!("index.dt", req);
-	req.params["recaptcha_challenge_field"] = '';
-	req.params["recaptcha_response_field"] = '';
+	string recaptcha = recaptchaHTML("6Lf4JusSAAAAACq9Fh9zyuByPCu8jLinglJygaT3");
+	res.renderCompat!("index.dt",
+		HTTPServerRequest, "req",
+		string, "recaptcha")
+		(req, recaptcha);
 }
-void post(HTTPServerRequest req, HTTPServerResponse res)
-{
 
+void success(HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.renderCompat!("success.dt",
+		HTTPServerRequest, "req")
+		(req);
+}
+
+void verify(HTTPServerRequest req, HTTPServerResponse res)
+{
+	string challenge = req.form["recaptcha_challenge_field"];
+	string response = req.form["recaptcha_response_field"];
+	if ( verifyRecaptcha("6Lf4JusSAAAAAIgeySdOHz12KfbyWicztJvyG-Yt", req.peer, challenge, response) == "true" )
+		res.redirect("/success");
+	else
+		//res.redirect(req.form["referer"]);
+		res.writeBody(cast(ubyte[]) "Not quite.", "text/plain");
+	logInfo("Challenge: %s", req.form["recaptcha_challenge_field"]);
+	//logInfo("Response: %s", req.form["recaptcha_response_field"]);
+	//logInfo("IP: %s", req.peer);
+	logInfo("Recaptcha Response: %s", verifyRecaptcha("6LeeAuYSAAAAAKDVlne-rXCFPNdLh4GWN-GKBSYg", req.peer, challenge, response));
 }
 
 shared static this()
 {
 	auto router = new URLRouter;
-	router.get("/", &index);
+	router.get("/", &index)
+				.post("/verify", &verify)
+				.get("/success", &success);
 
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
+	settings.bindAddresses = ["127.0.0.1"];
 
 	listenHTTP(settings, router);
 }
--- source/recaptcha/connect.d
+++ source/recaptcha/connect.d
@@ -1,9 +1,9 @@
-module recaptcha.connection;
+module recaptcha.connect;
 
+import std.utf;
 import std.string;
 import std.exception;
 import vibe.http.client;
-import vibe.data.json;
 import vibe.stream.operations;
 
 const string API_URL         = "http://www.google.com/recaptcha/api";
@@ -27,6 +27,7 @@ string recaptchaHTML(string publicKey, bool useSSL = false){
 	string html;
 	string server;
 
+	// Raise exception if publickey isn't present
 	enforceEx!RCX(publicKey, "Tried to use a null publicKey parameter.");
 	enforceEx!RCX(publicKey != "", "Tried to enter an empty publicKey parameter.");
 
@@ -49,7 +50,9 @@ string verifyRecaptcha(string privateKey, string ip, string challenge, string re
 	bool successful;
 	string[] apiResponse;
 	string error;
+	string query;
 	
+	// Raise exceptions for missing parameters
 	enforceEx!RCX(privateKey, "Tried to use a null private key parameter.");
 	enforceEx!RCX(privateKey != "", "Tried to enter an empty privateKey parameter.");
 	enforceEx!RCX(ip, "Tried to use a null remote IP parameter.");
@@ -63,17 +66,15 @@ string verifyRecaptcha(string privateKey, string ip, string challenge, string re
 
 	requestHTTP(VERIFY_URL, 
 		(scope req) {
-			string requestBody = "privatekey=%s&remoteip=%s&challenge=%s&response=%s",
-				privateKey, ip, challenge, response;
+			string requestBody;
+
+			query = "privatekey="~privateKey~"&remoteip="~ip~"&challenge="~challenge~"&response="~response;
+			
 			req.method = HTTPMethod.POST;
-			//req.port = 80;
-			req.headers["Content Type"] = "application/x-www-form-urlencoded";
-			req.contentType = "application/x-www-form-urlencoded";
-			req.headers["Content-Length"] = to!string(requestBody.length);
-			req.headers["Host"] = "www.google.com:80";
-			req.headers["Accept-Charset"] = "utf-8";
-			//req.headers["User-agent"] = "reCAPTCHA ";
-			req.writeBody(cast(ubyte[]) requestBody, "utf-8");
+			req.headers["User-agent"] = "reCAPTCHA Vibe.d";
+
+			requestBody = query;
+			req.writeBody(cast(ubyte[]) toUTF8(requestBody), "application/x-www-form-urlencoded;  charset=UTF-8");
 		},
 
 		(scope res) {
@@ -81,12 +82,58 @@ string verifyRecaptcha(string privateKey, string ip, string challenge, string re
 			if ( cast(string) apiResponse[0] == cast(string) "true" )
 				successful = true;
 			else
-				error = apiResponse[1];
+				successful = false;
 		}
 	);
 
-	if (successful)
-		return "true";
+	return (successful);
+}
+
+/**
+ * Check error returned by Recaptcha
+ */
+string testRecaptcha(string privateKey, string ip, string challenge, string response) {
+	bool successful;
+	string[] apiResponse;
+	string error;
+	string query;
+	
+	// Raise exceptions for missing parameters
+	enforceEx!RCX(privateKey, "Tried to use a null private key parameter.");
+	enforceEx!RCX(privateKey != "", "Tried to enter an empty privateKey parameter.");
+	enforceEx!RCX(ip, "Tried to use a null remote IP parameter.");
+	enforceEx!RCX(ip != "", "Tried to enter an empty remote IP parameter.");
+	enforceEx!RCX(challenge, "Tried to use a null challenge parameter.");
+	enforceEx!RCX(challenge != "", "Entered an empty challenge parameter.");
+	enforceEx!RCX(response, "Tried to use a null response parameter.");
+	enforceEx!RCX(response != "", "Entered an empty response parameter.");
+
+	successful = false;
+
+	requestHTTP(VERIFY_URL, 
+		(scope req) {
+			string requestBody;
+
+			query = "privatekey="~privateKey~"&remoteip="~ip~"&challenge="~challenge~"&response="~response;
+			
+			req.method = HTTPMethod.POST;
+			req.headers["User-agent"] = "reCAPTCHA Vibe.d";
+
+			requestBody = query;
+			req.writeBody(cast(ubyte[]) toUTF8(requestBody), "application/x-www-form-urlencoded;  charset=UTF-8");
+		},
+
+		(scope res) {
+			apiResponse = splitLines( res.bodyReader.readAllUTF8() );
+			if ( cast(string) apiResponse[0] == cast(string) "true" )
+				successful = true;
+			else
+				successful = false;
+		}
+	);
+
+	if(successful)
+		return apiResponse[0];
 	else
-		return error;
+		return apiResponse[1];
 }
